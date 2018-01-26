b7be4159ed226f294695f620394575b66305517c
Zaki Gandara <zakigandara@yahoo.co.id>
2018-01-26 15:42:58 +0700

    commitan kedua

--- wakwaw.c
+++ wakwaw.c
@@ -1,8 +1,20 @@
 #include<stdio.h>
 
 
+/**
+kalo string & di scanf sama di printf
+**/
+
 
 void main (){
 
+    char name;
     printf("Hello_world\n");
+
+    printf("please enter a string : ");
+    scanf("%s", &name);
+
+    printf("your string is: %s", &name);
+
+
 }
