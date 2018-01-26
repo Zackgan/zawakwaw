e1128b9eb6cd2fb96c4cbc13f6c65ab9c68df60b
Zaki Gandara <zakigandara@yahoo.co.id>
2018-01-26 16:03:00 +0700

    commitan ketiga

--- wakwaw.c
+++ wakwaw.c
@@ -8,7 +8,9 @@ kalo string & di scanf sama di printf
 
 void main (){
 
-    char name;
+    /**char name;
+
+    int pilihan;
     printf("Hello_world\n");
 
     printf("please enter a string : ");
@@ -16,5 +18,22 @@ void main (){
 
     printf("your string is: %s", &name);
 
+    **/
 
+
+    int nilai;
+
+
+    printf("Ketikan suatu nilai integer: ");
+    scanf("%d", &nilai);
+
+    if(nilai <0){
+        printf("nilai integer negatif %d", nilai);
+    }
+    else if(nilai>0){
+        printf("nilai integer positif %d", nilai);
+    }
+    else if (nilai==0){
+        printf("Nilai integer adalah %d", nilai);
+    }
 }
